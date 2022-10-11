93ff37adafc5ba6d6a3ce1c00c50a75e1a6eabde
Ivan Sidorenko <cidor.0712@gmail.com>
2022-10-11 17:23:31 +0500

    Task60

---
+++ Task60/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+Console.WriteLine("Введите X: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y: ");
+int column = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Z: ");
+int height = Convert.ToInt32(Console.ReadLine());
+int[,,] arr = new int[rows, column, height];
+
+void FillArrayRandom(int[,,] arr)
+{
+    int[] temp = new int[arr.GetLength(0) * arr.GetLength(1) * arr.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < arr.GetLength(0); x++)
+    {
+        for (int y = 0; y < arr.GetLength(1); y++)
+        {
+            for (int z = 0; z < arr.GetLength(2); z++)
+            {
+                arr[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+
+void PrintArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($"X({i}) Y({j}) Z({k})={arr[i, j, k]}; ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArrayRandom(arr);
+PrintArray(arr);
